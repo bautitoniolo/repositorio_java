d85de51dbec7d36eaeff34d905e0d76090e34bf7
Padel20x10 <padel20x10app@gmail.com>
2026-09-15 09:16:03 -0300

    second commit

--- hola.java
+++ hola.java
@@ -93,3 +93,72 @@ public class variables {
         return anioIngreso;
     }
  }
+
+ public class ano-bisiesto {
+    public static void main(String[] args) {
+        int year = 2024;
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            System.out.println(year + " es un año bisiesto.");
+        } else {
+            System.out.println(year + " no es un año bisiesto.");
+        }
+    }
+ }
+
+ public class contador {
+    public static void main(String [] args) {
+        int n = 10;
+        for (int i = 0 ; i < n ; i++) {
+            System.out.println("El valor de i es: " + i);   
+        }
+    }
+ }
+
+ public class suma { 
+    public static void main(String [] args)
+{
+    int n = 10;
+    int z = 20;
+
+    {
+     int sum = n + z;
+        System.out.println("La suma es: " + sum);
+    }
+}
+ }
+
+ public class persona {
+
+    public static void main(string [] args); {
+      Scanners edad = new Scanner(System.in);
+        System.out.println("Ingrese su edad: ");
+       int edad = edad.nextInt();
+ 
+    }
+ }
+
+public class calculoMatemtico { 
+    public static void calculoMatemtico (String [] args){
+      Scanners num1 = new Scanner(System.in);
+        System.out.println("Ingrese el primer numero: ");
+        int num1 = num1.nextInt();
+        Scanners num2 = new Scanner(System.in);
+        System.out.println("Ingrese el segundo numero: ");      
+        Scanners suma = new Scanner(System.in);
+        double suma = num1 + num2; 
+        System.out.println("La suma es: " + suma);
+    } 
+}
+
+
+
+public class mayorEdad {
+    public static void main(String [] args );
+    int n = 18; 
+    for (int n = 0 ; n < 10 ; n++) {
+        if (n >= 18) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("Es menor de edad");
+        }
+    }}
