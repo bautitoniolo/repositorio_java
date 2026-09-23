855f5b3af76b65896856cc993e2ec456ac3a8d1d
bauti <juanbautistatoniolo@gmail.com>
2026-09-23 11:29:40 -0300

    class escala

--- hola.java
+++ hola.java
@@ -190,3 +190,23 @@ public class enPantalla {
    System.out.println(datosExpuestos.nombre);
     }
 }
+
+public class retorno {
+     public static void main(String [] args){ 
+     for ( int i = 0 ; i < 10 ; i++) 
+       System.out.println("El valor de i es: " + i);
+     }
+}
+ 
+public class escala {
+    public static void main(String [] args ){
+        int i = 0;
+        while (i < 10 && i >= 0) { 
+            do {
+                System.out.pintln("El valor de i es: " + i);
+                i++;
+            } while (i < 10);
+        }
+    }
+
+}
